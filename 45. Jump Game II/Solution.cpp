67bb602118752b95f5f6434a18c7d084ad595a39
shozy <sshozye@gmail.com>
2022-06-14 17:32:54 +0200

    Added Jump Game II

--- 45. Jump Game II/Solution.cpp
+++ 45. Jump Game II/Solution.cpp
@@ -5,9 +5,26 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        int MAX_NUMS_LENGTH = 10000;
-        int dp[MAX_NUMS_LENGTH+1];
-        
-
+        if(n == 1){
+            return 0; // base case
+        }
+        int dp[n];
+        int new_reach;
+        dp[0] = 0;
+        int reach = 0;
+        for(int i = 0; i < n; i++){
+            if (i + nums[i] >= n-1){
+                dp[n-1] = dp[i]+1;
+                break;
+            }
+            if(i + nums[i] > reach){
+                new_reach = i + nums[i];
+                for(int j = reach+1; j <= new_reach; j++){
+                    dp[j] = dp[i] + 1;
+                }
+                reach = new_reach;
+            }
+        }
+        return dp[n-1]; 
     }
 };
