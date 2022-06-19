4beae8348f9236da218e7511bdfd16aeb0372d37
shozy <sshozye@gmail.com>
2022-06-19 18:54:29 +0200

    Added runtimes for 121

--- 121. Best Time to Buy and Sell/Solution.cpp
+++ 121. Best Time to Buy and Sell/Solution.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Runtime: 226 ms, faster than 23.54% of C++ online submissions for Best Time to Buy and Sell Stock.
+// Memory Usage: 93.4 MB, less than 52.07% of C++ online submissions for Best Time to Buy and Sell Stock.
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
